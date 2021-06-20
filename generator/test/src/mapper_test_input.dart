7b179771e375985854d0cfa2b1d73e2380abcd32
Nils Smolinski <nilssmol@gmail.com>
2021-06-20 17:30:05 +0200

    Add Test

--- generator/test/src/mapper_test_input.dart
+++ generator/test/src/mapper_test_input.dart
@@ -5,6 +5,7 @@ part 'simple_mapper_input.dart';
 part 'explicit_field_mapper_input.dart';
 part 'nested_mapper_input.dart';
 part 'injectable_mapper_input.dart';
+part 'list_mapper_input.dart';
 
 @ShouldThrow('theAnswer is not a class and cannot be annotated with @Mapper')
 @Mapper()
