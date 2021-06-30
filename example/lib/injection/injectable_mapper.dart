d17c4b1b5876d6cd84a0ff1467c7a064a16a47de
Nils Smolinski <nilssmol@gmail.com>
2021-06-30 17:16:06 +0200

    Example with constructor in injection example

--- example/lib/injection/injectable_mapper.dart
+++ example/lib/injection/injectable_mapper.dart
@@ -4,18 +4,41 @@ import 'package:smartstruct/smartstruct.dart';
 part 'injectable_mapper.mapper.g.dart';
 
 class InjectableSource {
+  final InjectableNestedSource nested;
+
+  InjectableSource(this.nested);
+}
+
+class InjectableNestedSource {
   final String text;
 
-  InjectableSource(this.text);
+  InjectableNestedSource(this.text);
 }
 
 class InjectableTarget {
+  final InjectableNestedTarget nested;
+
+  InjectableTarget(this.nested);
+}
+
+class InjectableNestedTarget {
   final String text;
 
-  InjectableTarget(this.text);
+  InjectableNestedTarget(this.text);
 }
 
 @Mapper(useInjection: true)
 abstract class InjectableMapper {
+  InjectableNestedMapper _nestedMapper;
+  InjectableMapper(this._nestedMapper);
+
   InjectableTarget fromSource(InjectableSource source);
+  InjectableNestedTarget fromNestedSource(InjectableNestedSource source) {
+    return _nestedMapper.fromSource(source);
+  }
+}
+
+@Mapper(useInjection: true)
+abstract class InjectableNestedMapper {
+  InjectableNestedTarget fromSource(InjectableNestedSource source);
 }
