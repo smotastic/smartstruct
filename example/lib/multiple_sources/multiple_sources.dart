1749998355becdcf10632a7e7bfbcdee5b02ca86
smotastic <nilssmol@gmail.com>
2021-10-04 18:30:18 +0200

    Extend Example

--- example/lib/multiple_sources/multiple_sources.dart
+++ example/lib/multiple_sources/multiple_sources.dart
@@ -6,8 +6,9 @@ class Target {
   final String text1;
   final String text2;
   final String text3;
+  final String allText;
 
-  Target(this.text1, this.text2, this.text3);
+  Target(this.text1, this.text2, this.text3, this.allText);
 }
 
 class Source {
@@ -27,5 +28,9 @@ class Source3 {
 
 @Mapper()
 abstract class MultipleSourcesMapper {
+  @Mapping(source: allText, target: 'allText')
   Target fromSource(Source source, Source2 source2, Source3 source3);
 }
+
+String allText(Source source, Source2 source2, Source3 source3) =>
+    source.text1 + source2.text2 + source3.text3;
--- example/lib/multiple_sources/multiple_sources.mapper.g.dart
+++ example/lib/multiple_sources/multiple_sources.mapper.g.dart
@@ -11,7 +11,8 @@ class MultipleSourcesMapperImpl extends MultipleSourcesMapper {
 
   @override
   Target fromSource(Source source, Source2 source2, Source3 source3) {
-    final target = Target(source.text1, source2.text2, source3.text3);
+    final target = Target(source.text1, source2.text2, source3.text3,
+        allText(source, source2, source3));
     return target;
   }
 }
