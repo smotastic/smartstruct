0d8e58e0f9d78661e2f3d602675f206c2b2915c1
flowing_sky <flowing_sky@126.com>
2022-06-26 16:10:53 +0800

    Add the test case for the features that ingoring the nulliability on the method matching of nested mapping and the hanlding with the null value when apply the nested mapping.

--- generator/test/mapper_test.dart
+++ generator/test/mapper_test.dart
@@ -22,6 +22,7 @@ const _expectedAnnotatedTests = {
   'SimpleMapper',
   'ExplicitFieldMapper',
   'NestedMapper',
+  'NullableNestedMapper',
   'InjectableMapper',
   'ListMapper',
   "CollectionMapper",
