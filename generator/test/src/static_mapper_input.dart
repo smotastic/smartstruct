8a5f58cac39f6c2d912fdfb8b3902ede0f8d7377
smotastic <nilssmol@gmail.com>
2022-10-07 17:15:13 +0200

    Tests added

--- generator/test/src/static_mapper_input.dart
+++ generator/test/src/static_mapper_input.dart
@@ -1,10 +1,15 @@
 part of 'mapper_test_input.dart';
 
+enum StaticEnumTarget {
+  ONE,TWO,THREE
+}
+
 class StaticMappingTarget {
   final String text;
   final num number;
+  final StaticEnumTarget foo;
 
-  StaticMappingTarget(this.text, this.number);
+  StaticMappingTarget(this.text, this.number, this.foo);
 }
 
 class StaticMappingSource {
@@ -33,16 +38,26 @@ StaticMappingTarget _$fromSourceStatic(StaticMappingSource source) {
   final staticmappingtarget = StaticMappingTarget(
     source.text,
     source.number,
+    StaticMappingMapper.mapEnum(source),
   );
   return staticmappingtarget;
 }
 ''')
 abstract class StaticMappingMapper {
-  static String someRandomMethodWhichShouldBeIgnored() => "ignore me";
-  StaticMappingTarget fromSourceNormal(StaticMappingSource source);
+  static String ignoredPrimitiveTypeMethod() => "ignore me"; // should not generate extra helper
+
+  // these helpers should not be generated as they are either primitive, or are not explicitly declared to be generated via the StaticMapping Annotation
+  static StaticEnumTarget mapEnum(StaticMappingSource source) => StaticEnumTarget.ONE;
+  static List<StaticEnumTarget> mapEnumList(StaticMappingSource source) => [StaticEnumTarget.ONE, StaticEnumTarget.THREE];
+  static Set<String> mapSomeSet(StaticMappingSource source) => {"1", "2"};
+
+  // this helper should be generated as it is explicitly configured to do so via the StaticMapping Annotation
   @StaticMapping()
+  @Mapping(source: mapEnum, target: 'foo')
   static StaticMappingTarget fromSourceStatic(StaticMappingSource source) => _$fromSourceStatic(source);
+
+  StaticMappingTarget fromSourceNormal(StaticMappingSource source);
 }
 
 // just so the test compiles
-StaticMappingTarget _$fromSourceStatic(StaticMappingSource source) => StaticMappingTarget("text", 1);
+StaticMappingTarget _$fromSourceStatic(StaticMappingSource source) => StaticMappingTarget("text", 1, StaticEnumTarget.THREE);
